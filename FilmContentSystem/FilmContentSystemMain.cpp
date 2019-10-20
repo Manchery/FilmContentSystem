ab9c4ba796d70cc7921e652a3b0f1b0289fdf2e7
Jialong Wu <Manchery@outlook.com>
2019-10-20 18:57:26 +0800

    Done: scanning a dir

--- FilmContentSystem/FilmContentSystemMain.cpp
+++ FilmContentSystem/FilmContentSystemMain.cpp
@@ -1,28 +1,78 @@
 #include "FilmContentSystemApplication.h"
 #include <fstream>
 #include <sstream>
+#include <cstring>
+
 #include <locale>
 #include <codecvt>
+
+#include <direct.h>
+#include <io.h>
+
 using namespace std;
+
+bool endsWith(const char * str, const char *end) {
+	int len1 = strlen(str), len2 = strlen(end);
+	return len1 >= len2 && 0 == strcmp(str + len1 - len2, str) == 0;
+}
 
 void _main() {
 	FilmContentSystemApplication app; 
 
-	app.initDictionary("C:\\Users\\Manchery\\Desktop\\jieba\\dict2.txt", "C:\\Users\\Manchery\\Desktop\\jieba\\HMM3.txt");
-	auto info = app.extractInfo("C:\\Users\\Manchery\\Desktop\\实验1材料\\input\\11.html");
+	const char dictFile[] = "C:\\Users\\Manchery\\Desktop\\jieba\\dict2.txt";
+	const char hmmFile[] = "C:\\Users\\Manchery\\Desktop\\jieba\\HMM3.txt";
+	const char inputDir[] = "C:\\Users\\Manchery\\Desktop\\实验1材料\\input\\";
+	const char outputDir[] = "C:\\Users\\Manchery\\Desktop\\实验1材料\\output\\";
+	_mkdir(outputDir);
 
-	std::wofstream wfout("C:\\Users\\Manchery\\Desktop\\output.txt");
-	wfout.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<wchar_t>));
-	wfout << info;
-	wfout.close();
+	app.initDictionary(dictFile, hmmFile);
 
-	wfout.open("C:\\Users\\Manchery\\Desktop\\output2.txt");
-	wfout.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<wchar_t>));
-	CharStringLink cuts = app.divideWords(info.introduction());
-	for (auto it = cuts.begin(); it != cuts.end(); ++it) {
-		wfout << *it << std::endl;
+	_finddata_t file;
+	long lf; const int MAX_FILE_NAME_LEN = 1000;
+	char inputReg[MAX_FILE_NAME_LEN] = { 0 };
+	strcat(strcat(inputReg, inputDir), "*");
+	if ((lf = _findfirst(inputReg, &file)) == -1) {
+		std::cerr << inputDir << " not found!" << std::endl;
 	}
-	wfout.close();
+	else {
+		while (_findnext(lf, &file) == 0) {
+			if (strcmp(file.name, ".") == 0 || strcmp(file.name, "..") == 0)
+				continue;
+			if (!endsWith(file.name, ".html"))
+				continue;
+
+			std::cerr << "Found " << file.name << "..." << std::endl;
+
+			char baseName[MAX_FILE_NAME_LEN] = { 0 };
+			strncpy(baseName, file.name, strlen(file.name) - 5);
+			char filePath[MAX_FILE_NAME_LEN] = { 0 };
+			strcat(strcat(filePath, inputDir), file.name);
+			char infoFile[MAX_FILE_NAME_LEN] = { 0 };
+			strcat(strcat(strcat(infoFile, outputDir), baseName), ".info");
+			char txtFile[MAX_FILE_NAME_LEN] = { 0 };
+			strcat(strcat(strcat(txtFile, outputDir), baseName), ".txt");
+
+			/*------------------------------------------------------------------------------------*/
+
+			auto info = app.extractInfo(filePath);
+
+			std::wofstream wfout(infoFile);
+			wfout.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<wchar_t>));
+			wfout << info;
+			wfout.close();
+
+			wfout.open(txtFile);
+			wfout.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<wchar_t>));
+			CharStringLink cuts = app.divideWords(info.introduction());
+			for (auto it = cuts.begin(); it != cuts.end(); ++it) {
+				wfout << *it << std::endl;
+			}
+			wfout.close();
+
+			/*------------------------------------------------------------------------------------*/
+		}
+	}
+	_findclose(lf);
 }
 
 int main() {
